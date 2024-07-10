dffc28f9c1b8df3984ad4c144e78536b63509732
Albert <wtk2868280@163.com>
2024-07-10 14:57:52 +0800

    调整API

--- FootballManagerBackend/Controllers/PlayerDisplayController.cs
+++ FootballManagerBackend/Controllers/PlayerDisplayController.cs
@@ -33,4 +33,33 @@ namespace FootballManagerBackend.Controllers
             return Ok(result);
         }
     }
+
+    [Route("v1/playermanage")]
+    public class PlayerController : ControllerBase
+    {
+        private readonly OracleDbContext _context;
+
+        public PlayerController(OracleDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> Get()
+        {
+            string query = "SELECT player_id, player_name, team_id FROM players";
+            List<Dictionary<string, object>> result = await _context.ExecuteQueryAsync(query);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            string query = "SELECT * FROM players WHERE player_id = :id";
+            var parameters = new Dictionary<string, object> { { "id", id } };
+
+            List<Dictionary<string, object>> result = await _context.ExecuteQueryAsync(query, parameters);
+            return Ok(result);
+        }
+    }
 }
