10028e030265483b15aa9683394f08a897c5b00c
sxm <59765936+windfall2321@users.noreply.github.com>
2024-07-10 14:14:19 +0800

    Add files via upload
    支持post通过json添加新球队信息

--- FootballManagerBackend/Controllers/TeamController.cs
+++ FootballManagerBackend/Controllers/TeamController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -30,6 +31,52 @@ namespace FootballManagerBackend.Controllers
             List<Dictionary<string, object>> result = await _context.ExecuteQueryAsync(query, parameters);
             return Ok(result);
         }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] JsonElement teamElement)
+        {
+            string query = "INSERT INTO teams (team_name, established_date, head_coach, city, team_id) VALUES (:name, :checkdate, :coach, :city, :id)";
+
+            var parameters = new Dictionary<string, object>();
+
+            // 从 JsonElement 中获取值，并进行类型转换
+            foreach (var property in teamElement.EnumerateObject())
+            {
+                switch (property.Name.ToLower()) 
+                {
+                    case "team_name":
+                        parameters.Add("name", property.Value.GetString());
+                        break;
+                    case "established_date":
+                        if (DateTime.TryParse(property.Value.GetString(), out DateTime dateValue))
+                        {
+                            parameters.Add("checkdate", dateValue);
+                        }
+                        else
+                        {
+                            // 返回错误信息
+                            Console.WriteLine($"Invalid date format for established_date: {property.Value.GetString()}");
+                            
+                        }
+                        break;
+                    case "head_coach":
+                        parameters.Add("coach", property.Value.GetString());
+                        break;
+                    case "city":
+                        parameters.Add("city", property.Value.GetString());
+                        break;
+                    case "team_id":
+                        parameters.Add("id", property.Value.GetInt32());
+                        break;
+                    default:
+
+                        break;
+                }
+            }
+
+            await _context.ExecuteNonQueryAsync(query, parameters);
+            return CreatedAtAction(nameof(Get), new { Teamid = parameters["id"] }, parameters);
+        }
+
         /*[HttpGet]
         public IEnumerable<string> Get()
         {
