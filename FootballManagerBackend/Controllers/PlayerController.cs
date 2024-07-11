6bad6ebfed72fa1dd16c93bf390d3fc9cef51240
Albert <wtk2868280@163.com>
2024-07-11 10:13:51 +0800

    修改路径

--- FootballManagerBackend/Controllers/PlayerController.cs
+++ FootballManagerBackend/Controllers/PlayerController.cs
@@ -7,16 +7,16 @@ namespace FootballManagerBackend.Controllers
 {
     [ApiController]
     [Route("v1/player")]
-    public class DataController : ControllerBase
+    public class PlayerController : ControllerBase
     {
         private readonly OracleDbContext _context;
 
-        public DataController(OracleDbContext context)
+        public PlayerController(OracleDbContext context)
         {
             _context = context;
         }
 
-        [HttpGet("all")] // GET /v1/player/all or GET /v1/player/all?teamid=*
+        [HttpGet("displayall")] // GET /v1/player/displayall or GET /v1/player/displayall?teamid=*
         public async Task<IActionResult> Get(string? teamid = null)
         {
             string query = "SELECT * FROM players ORDER BY player_name";
@@ -34,20 +34,20 @@ namespace FootballManagerBackend.Controllers
             }
         }
 
-        [HttpGet("{Playerid}")] // GET /v1/player/*
-        public async Task<IActionResult> Get(int Playerid)
+        [HttpGet("displayone")] // GET /v1/player/displayone?playerid=*
+        public async Task<IActionResult> Get(int playerid)
         {
             string query = "SELECT * FROM players WHERE player_id = :Playerid";
-            var parameters = new Dictionary<string, object> { { "Playerid", Playerid } };
+            var parameters = new Dictionary<string, object> { { "Playerid", playerid } };
 
             List<Dictionary<string, object>> result = await _context.ExecuteQueryAsync(query, parameters);
             return Ok(result);
         }
 
-        [HttpPost] // POST /v1/player
+        [HttpPost("add")] // POST /v1/player/add
         public async Task<IActionResult> Post([FromBody] JsonElement playerElement)
         {
-            string query = "INSERT INTO players (player_id, player_name, birthday, team_id, role, used_foot, health_state, rank, game_state, trans_state, is_show) VALUES (:id, :name, :checkdate, :team, :rolein, :foot, :health, :ranking, :game, :trans, :show)";
+            string query = "INSERT INTO players (player_id, player_name, birthday, team_id, role, used_foot, health_state, rank, game_state, trans_state, is_show) VALUES (:player_id, :player_name, :birthday, :team_id, :role, :used_foot, :health_state, :rank, :game_state, :trans_state, :is_show)";
 
             var parameters = new Dictionary<string, object>();
 
@@ -56,15 +56,15 @@ namespace FootballManagerBackend.Controllers
                 switch (property.Name.ToLower())
                 {
                     case "player_id":
-                        parameters.Add("id", property.Value.GetInt32());
+                        parameters.Add("player_id", property.Value.GetInt32());
                         break;
                     case "player_name":
-                        parameters.Add("name", property.Value.GetString());
+                        parameters.Add("player_name", property.Value.GetString());
                         break;
                     case "birthday":
                         if (DateTime.TryParse(property.Value.GetString(), out DateTime dateValue))
                         {
-                            parameters.Add("checkdate", dateValue);
+                            parameters.Add("birthday", dateValue);
                         }
                         else
                         {
@@ -73,28 +73,28 @@ namespace FootballManagerBackend.Controllers
                         }
                         break;
                     case "team_id":
-                        parameters.Add("team", property.Value.GetInt32());
+                        parameters.Add("team_id", property.Value.GetInt32());
                         break;
                     case "role":
-                        parameters.Add("rolein", property.Value.GetString());
+                        parameters.Add("role", property.Value.GetString());
                         break;
                     case "used_foot":
-                        parameters.Add("foot", property.Value.GetInt32());
+                        parameters.Add("used_foot", property.Value.GetInt32());
                         break;
                     case "health_state":
-                        parameters.Add("health", property.Value.GetInt32());
+                        parameters.Add("health_state", property.Value.GetInt32());
                         break;
                     case "rank":
-                        parameters.Add("ranking", property.Value.GetInt32());
+                        parameters.Add("rank", property.Value.GetInt32());
                         break;
                     case "game_state":
-                        parameters.Add("game", property.Value.GetInt32());
+                        parameters.Add("game_state", property.Value.GetInt32());
                         break;
                     case "trans_state":
-                        parameters.Add("trans", property.Value.GetInt32());
+                        parameters.Add("trans_state", property.Value.GetInt32());
                         break;
                     case "is_show":
-                        parameters.Add("show", property.Value.GetInt32());
+                        parameters.Add("is_show", property.Value.GetInt32());
                         break;
                     default:
                         break;
