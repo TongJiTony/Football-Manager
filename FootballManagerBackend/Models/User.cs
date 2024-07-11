648ed8de94b3c66db9b5625d843e1ecb8261dbcd
zjz <1034597947@qq.com>
2024-07-11 12:16:39 +0800

    user表的添加、登录并鉴权、修改密码功能

---
+++ FootballManagerBackend/Models/User.cs
@@ -0,0 +1,70 @@
+﻿namespace FootballManagerBackend.Models
+{
+    public class User
+    {
+        private int userId;
+        private string userName;
+        private string userRight;
+        private string userPassword;
+        private long userPhone;
+        private string icon;
+
+        public int UserId
+        {
+            get { return userId; }
+            set { userId = value; }
+        }
+
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+
+        public string UserRight
+        {
+            get { return userRight; }
+            set { userRight = value; }
+        }
+
+        public string UserPassword
+        {
+            get { return userPassword; }
+            set { userPassword = value; }
+        }
+
+        public long UserPhone
+        {
+            get { return userPhone; }
+            set { userPhone = value; }
+        }
+
+        public string Icon
+        {
+            get { return icon; }
+            set { icon = value; }
+        }
+    }
+
+    public class LoginRequest
+    {
+        public long user_id { get; set; }
+        public string user_password { get; set; }
+    }
+
+    public class ChangePasswordRequest
+    {
+        public long user_id { get; set; }
+        public string user_password { get; set; }
+        public string new_password {  get; set; }
+    }
+
+    public class JwtConfig
+    {
+        public string Key { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        // 其他可能的JWT配置属性
+    }
+
+}
